9013a3b6e095ef8c0df8ea6f37d23c557f4ce799
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-05-06 12:39:11 -0700

    Implemented projects hero

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function Projects() {
   return (
-    <div>
-      
-    </div>
+   
+      <section className='projects__container'>
+
+
+
+        <div className='projects__colorBackground order-1'>
+        </div>
+
+        <div className='projects__title'>
+          <h1>projects</h1>
+        </div>
+
+      </section>
+  
   )
 }
