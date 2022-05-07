52dffcef043da67ed0d15235d9f77df339bb7be8
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-05-06 20:09:19 -0700

    Made page header mobile responsive.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,17 +5,26 @@ export default function Projects() {
     <section className="projects__container">
 
       <div className="projects__title">
+        <div className="projects__corner-top"></div>
+        <div className="projects__corner-left"></div>
+
         <h1 className="projects__heading">projects</h1>
-        {/* <div className="projects__corner"></div> */}
+
+        <div className="projects__corner-rContainer">
+          <div className="projects__corner-right"></div>       
+          <div className="projects__corner-bottom"></div> 
+        </div>
       </div>
 
-      {/* <div className="projects__colorBackground "> */}
-
-      <p className="p-5 ml-5">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-      </p>
-
-      {/* </div> */}
+  
+      <div className="row align-items-end projects__hero-columns">
+      <div className="col-sm-12 col-md-6 col-lg-6">
+            <p className="p-5 ml-5 projects__overview">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            </p>
+      </div>
+      <div className="col-sm-12 col-md-6 col-lg-6"></div>
+      </div>
 
 
     </section>
