bc6242dd9be784c92fac248a304b36e97baa3091
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-07-01 16:24:08 -0700

    Troubleshooting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 // const PORT = process.env.PORT || 3001;
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // Loads required dependencies
 const express = require("express");
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 // app.listen(5000, () => console.log("Server Running"));
-app.listen(PORT, () => console.log("Server Running"));
+app.listen(port, () => console.log("Server Running"));
 
 // const oAuth2Client = new OAuth2(
 //   process.env.CLIENT_ID,
