9c8ae83561d089f1e6d884416c0da4a45e8e1077
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-07-01 18:47:49 -0700

    Troubleshooting

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use("/", router);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build'));
 });
 
 // app.listen(5000, () => console.log("Server Running"));
